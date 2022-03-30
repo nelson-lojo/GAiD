f2cb0415adfee6525b7b3435dc83720474e00257
Nelson Lojo <nalsonklojo@gmail.com>
2022-03-29 19:47:35 -0700

    maybe just do exactly as the error says?

--- utils/tracking.py
+++ utils/tracking.py
@@ -86,7 +86,7 @@ class Tracker:
 
     @classmethod
     def getCountedRuns(cls, name: str, age: timedelta) -> int:
-        countStmt = ( sa.select(sa.func.count('*')). \
+        count = cls._db.select(sa.func.count('*')). \
             select_from(cls.table). \
             where(
                 sa.and_(
@@ -94,10 +94,9 @@ class Tracker:
                     cls.table.c.time > (datetime.now() - age)
                 )
             ).scalar()
-        )
 
-        with cls._db.begin() as conn:
-            count = conn.execute(countStmt)
+        # with cls._db.begin() as conn:
+        #     count = conn.execute(countStmt)
 
         return count
         
@@ -105,9 +104,9 @@ class Tracker:
     @classmethod
     def addRun(cls, label, arguments, kwarguments) -> None:
         # consider storing search words
-        stmt = ( sa.insert(cls.table).values(label=label, time=datetime.now()) )
+        cls._db.insert(cls.table).values(label=label, time=datetime.now())
 
-        with cls._db.begin() as conn:
-            conn.execute(stmt)
+        # with cls._db.begin() as conn:
+        #     conn.execute(stmt)
 
         return
