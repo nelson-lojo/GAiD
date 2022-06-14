d42ee7e7247607e9a63b7bde1553879c9ecded23
nelcel <nalsonklojo@gmail.com>
2022-06-13 20:24:30 -0700

    pls no overlapping column names

--- cogs/modularity.py
+++ cogs/modularity.py
@@ -19,7 +19,7 @@ class Modularity(commands.Cog):
         self.table = sa.Table(
             'modularity-authorized', md,
             sa.Column('name', sa.TEXT),
-            sa.Column('user_id', sa.TEXT, primary_key=True)
+            sa.Column('discord_user_id', sa.TEXT, primary_key=True)
         )
         self.table.create(Modularity.db, checkfirst=True)
 
@@ -44,7 +44,7 @@ class Modularity(commands.Cog):
         # condition: `usr is owner`` or `usr in authorized_table`
         return str(user) == Modularity.owner or \
                 self.table.select().where(
-                    self.table.c.user_id == user.id
+                    self.table.c.discord_user_id == user.id
                     ).execute().rowcount > 1 
 
     @commands.command(name='load', aliases=['lm'], brief='Load a module', pass_context=True)
@@ -115,7 +115,7 @@ class Modularity(commands.Cog):
 
         for user in users: 
             try:
-                self.table.insert().values(name=user.name, user_id=user.id).execute()
+                self.table.insert().values(name=user.name, discord_user_id=user.id).execute()
                 added.append(user)
             except sa.exc.IntegrityError:
                 already.append(user)
@@ -134,7 +134,7 @@ class Modularity(commands.Cog):
         users: List[discord.User] = await self.getUsers(mentions)
 
         for user in users: 
-            self.table.delete().where(self.table.c.user_id == user.id).execute()
+            self.table.delete().where(self.table.c.discord_user_id == user.id).execute()
 
         await Modularity.sendListMessage(context, "*User{s} {lst} now not authorized*", users)
 
