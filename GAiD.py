ad4896574b28b6aa1a3fe9af106e66e7331f23d0
Nelson Lojo <nalsonklojo@gmail.com>
2022-09-06 19:03:21 -0700

    new discord.py launch paradigm

--- GAiD.py
+++ GAiD.py
@@ -1,13 +1,14 @@
 from utils.keys import keys
 from sys import argv
 from discord.ext.commands import Bot
+import discord
 
 if __name__ == '__main__':
 
     discordToken = argv[1]
 
     prefix = keys['prefix']
-    gaid = Bot(command_prefix=prefix) 
+    gaid = Bot(command_prefix=prefix, intents=discord.Intents.default()) 
 
     for ext in ['search', 'modularity']:
         gaid.load_extension(f'cogs.{ext}')
